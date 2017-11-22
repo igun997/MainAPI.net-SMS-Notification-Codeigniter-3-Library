1d38cfffc3252d550281a1c02c2a20b74410915b
Indra Gunanda <indra.gunanda@gmail.com>
2017-11-23 02:04:47 +0700

    Init Commit

---
+++ application/libraries/Sendsms.php
@@ -0,0 +1,110 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Sendsms
+ * 
+ * Send SMS by Api mainapi.net
+ * 
+ * @package Sendsms
+ * @author SystemFive <indra.gunanda@gmail.com>
+ * @version 0.0.1
+ */
+require_once APPPATH . 'libraries/curl/curl.php';
+
+class Sendsms {
+
+    private $client_id;
+    private $secret_id;
+    private $response;
+    private $msdn;
+    private $content;
+    private $init;
+
+    /**
+     * Constructor function
+     *
+     * @return Sendsms 
+     */
+    public function __construct() {
+        $ci = & get_instance();
+        $this->init = $ci;
+    }
+
+    function getClient_id() {
+        return $this->client_id;
+    }
+
+    function getSecret_id() {
+        return $this->secret_id;
+    }
+
+    function getResponse() {
+        return $this->response;
+    }
+
+    function getMsdn() {
+        return $this->msdn;
+    }
+
+    function getContent() {
+        return $this->content;
+    }
+
+    function setClient_id($client_id) {
+        $this->client_id = $client_id;
+    }
+
+    function setSecret_id($secret_id) {
+        $this->secret_id = $secret_id;
+    }
+
+    function setResponse($response) {
+        $this->response = $response;
+    }
+
+    function setMsdn($msdn) {
+        $this->msdn = $msdn;
+    }
+
+    function setContent($content,$filter="true") {
+        if (!$filter) {
+            $content = strip_tags($content, "<p>");
+            $content = str_replace('<p>', '', $isi);
+            $content = str_replace('</p>', '', $isi);
+        } 
+        $this->content = $content;
+    }
+
+    private function _genToken() {
+
+        $base = "Basic " . base64_encode($this->client_id . ":" . $this->secret_id);
+        $instances = new Curl();
+        $instances->headers['Authorization'] = $base;
+        $response = $instances->post("https://api.mainapi.net/token", array("grant_type" => "client_credentials"));
+        $data = json_decode($response->body);
+        $ap = (isset($data->access_token)) ? $data->access_token : null;
+        return $ap;
+    }
+
+    function sendSMS() {
+        $ap = $this->_genToken();
+        if ($ap != null) {
+            $instances = new Curl();
+            $instances->headers["Content-Type"] = "application/x-www-form-urlencoded";
+            $instances->headers["Accept"] = "application/json";
+            $instances->headers["Authorization"] = "Bearer " . $ap;
+            $res = $instances->post("https://api.mainapi.net/smsnotification/1.0.0/messages", array("msisdn" => $this->msdn, "content" => $this->content));
+            return $res;
+        } else {
+            return (object) array("code" => 0, "msg" => "Wrong Client ID or Secret ID");
+        }
+    }
+    
+
+}
+
+/* End of file Sendsms.php */
+/* Location: ./application/libraries/Sendsms.php */
